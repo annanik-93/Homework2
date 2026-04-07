6302181b2eb7ac9373cc89c3845fbb75bfa7ca44
Anastacia <grofman93@gmail.com>
2026-04-07 12:25:49 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,85 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }
+
+        int dog = 8;
+        double cat = 3.6;
+        long paper = 763789;
+
+        System.out.println ();
+        System.out.println ("Задание 1");
+        System.out.println ("dog = " + dog);
+        System.out.println ("cat = " + cat);
+        System.out.println ("paper = " + paper);
+
+        int dog1 = dog+4;
+        double cat1 = cat+4;
+        long paper1 = paper+4;
+        System.out.println ();
+        System.out.println ("Задание 2");
+        System.out.println ("dog1 = " + dog1);
+        System.out.println ("cat1 = " + cat1);
+        System.out.println ("paper1 = " + paper1);
+
+        double dog2 = dog1-3.5;
+        double cat2 = cat1-1.6;
+        long paper2 = paper1-7639;
+        System.out.println ();
+        System.out.println ("Задание 3");
+        System.out.println ("dog2 = " + dog2);
+        System.out.println ("cat2 = " + cat2);
+        System.out.println ("paper2 = " + paper2);
+
+        int friend = 19;
+        int friend1 = friend+2;
+        int friend2 = friend1/7;
+        System.out.println ();
+        System.out.println ("Задание 4");
+        System.out.println (friend);
+        System.out.println (friend1);
+        System.out.println (friend2);
+
+
+        double frog = 3.5;
+        double frog1 = frog*10;
+        double frog2 = frog1/3.5;
+        double frog3 = frog2+4;
+        System.out.println ();
+        System.out.println ("Задание 5");
+        System.out.println (frog);
+        System.out.println (frog1);
+        System.out.println (frog2);
+        System.out.println (frog3);
+
+        double boxerMass1 = 78.2;
+        double boxerMass2 = 82.7;
+        double totalMass = boxerMass1 + boxerMass2;
+        double difference = totalMass - boxerMass1;
+        System.out.println ();
+        System.out.println ("Задание 6");
+        System.out.println ("boxerMass1 = " + boxerMass1);
+        System.out.println ("boxerMass2 = " + boxerMass2);
+        System.out.println ("totalMass = " + totalMass);
+        System.out.println ("difference = " + difference);
+
+        double remainder = boxerMass2 % boxerMass1;
+        System.out.println ();
+        System.out.println ("Задание 7");
+        System.out.println ("remainder = " + remainder);
+
+        int ollTime= 640;
+        int employeeTime1 = 8;
+        int employee1 = ollTime/employeeTime1;
+        int employee2 = employee1+94;
+        int employeeTime2 =ollTime/employee2;
+        System.out.println ();
+        System.out.println ("Задание 8");
+        System.out.println ("Всего работников в компании  = " + employee1 + " человек");
+        System.out.println ("Если в компании работает " + employee2 + " человек, то всего " + employeeTime2 + " часа работы может быть поделено между сотрудниками ");
+
+    }}
 
 
 
-}
+
+
